34dc6865d34e97eb3392575f8d8f550adc79fd36
Dontizy <raphaeldonatus9@gmail.com>
2026-06-13 12:42:27 -0400

    allowed all origin cors

--- src/index.ts
+++ src/index.ts
@@ -18,10 +18,7 @@ const app = express();
 
 const port = Number(process.env.PORT) || 3000;
 app.use(
-    cors({
-        origin: process.env.CLIENT_URL,
-        credentials: true,
-    })
+    cors()
 );
 app.use(express.urlencoded({extended:true}))
 app.use(express.json({ limit: "10mb" }));
